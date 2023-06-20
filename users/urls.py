0c5f438b87ef9ca76f5660b08f3d4d0c1c013b47
Nahuan Barreto <nahuan89@gmail.com>
2023-06-20 04:26:40 -0300

    refactoring

--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "users"
-
-urlpatterns = [
-    path("create-post/", views.create_post, name="create_post"),
-    path("", views.post_list, name="post_list"),
-]
